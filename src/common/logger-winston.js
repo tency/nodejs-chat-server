618d63a39e9164926bf41bfcc88b83a6f7b31d25
tency <tencygjq@163.com>
2019-12-22 14:12:59 +0800

    tests

--- src/common/logger-winston.js
+++ src/common/logger-winston.js
@@ -69,10 +69,10 @@ class Logger {
 
     configWinston() {
         winston.configure({
-            format: format.combine(
-                format.splat(),
-                format.simple(),
-            ),
+            // format: format.combine(
+            //     format.splat(),
+            //     format.simple(),
+            // ),
             transports: [
                 new(winston.transports.Console)({
                     timestamp: function () {
