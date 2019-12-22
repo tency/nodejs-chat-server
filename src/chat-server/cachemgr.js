48b4d5227cf3c682fddb13795e06173dc60b8486
tency <tencygjq@163.com>
2019-12-22 14:34:01 +0800

    重命名文件

---
+++ src/chat-server/cachemgr.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const redis = require("redis");
+const config = require("../config");
+const log = logger.getLogger("cachemgr");
+
+class CacheMgr {
+    constructor() {
+        this.cache = null;
+    }
+
+    init() {
+        return new Promise((resolve, reject) => {
+            this.connectRedis((cache) => {
+                this.cache = cache;
+                log.info("redis connect...");
+                resolve();
+            });
+        });
+    }
+
+    connectRedis(callback) {
+        var client = redis.createClient(config.redisPort, config.redisHost);
+        client.auth(config.redisAuth || '');
+        client.select(config.redisId, function (err) {
+            if (err) {
+                log.error(err);
+                process.exit(-1);
+            }
+        });
+
+        client.on('ready', function () {
+            callback && callback(client);
+        });
+
+        client.on('error', function (e) {
+            log.error(e);
+        });
+    }
+}
+
+let cacheMgr = global.cacheMgr || new CacheMgr();
+module.exports = cacheMgr;
